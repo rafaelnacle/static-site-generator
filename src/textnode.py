65e4b11af01b6ff83a39d8aa6df082809a0b7a40
Rafael Nacle <rafaelnacle@hotmail.com>
2024-03-08 17:25:23 -0300

    fixed eq method and added text_types

--- src/textnode.py
+++ src/textnode.py
@@ -1,10 +1,18 @@
+text_type_text = "text"
+text_type_bold = "bold"
+text_type_italic = "italic"
+text_type_code = "code"
+text_type_link = "link"
+text_type_image = "image"
+
+
 class TextNode:
     def __init__(self, text, text_type, url=None):
         self.text = text
         self.text_type = text_type
         self.url = url
 
-    def eq(self, other):
+    def __eq__(self, other):
         return (
             self.text_type == other.text_type
             and self.text == other.text

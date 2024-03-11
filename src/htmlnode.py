3680565bb49001f88c02c79752202cc29dc02f64
Rafael Nacle <rafaelnacle@hotmail.com>
2024-03-11 10:31:05 -0300

    fixed LeafNode constructor name, added ParentNode class

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -21,7 +21,7 @@ class HTMLNode:
 
 
 class LeafNode(HTMLNode):
-    def __init(self, tag, value, props=None):
+    def __init__(self, tag, value, props=None):
         super().__init__(tag, value, None, props)
 
     def to_html(self):
@@ -31,5 +31,23 @@ class LeafNode(HTMLNode):
             return self.value
         return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
 
-        def __repr__(self):
-            return f"LeafNode({self.tag}, {self.value}, {self.props})"
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
+
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("Invalid HTML: no tag")
+        if self.children is None:
+            raise ValueError("Invalid HTML: no children")
+        children_html = ""
+        for child in self.children:
+            children_html += child.to_html()
+        return f"<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}>"
+
+    def __repr__(self):
+        return f"ParentNode({self.tag}, children: {self.children}, {self.props})"

bd3216dea62e25562eaca12d8372d178e5eecbd4
Rafael Nacle <rafaelnacle@hotmail.com>
2024-03-11 11:55:04 -0300

    added extract_markdown_images added extract_markdown_links functions

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -28,3 +28,15 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                 split_nodes.append(TextNode(sections[i], text_type))
         new_nodes.extend(split_nodes)
     return new_nodes
+
+
+def extract_markdown_images(text):
+    pattern = r"!\[(.*?)\]\((.*?)\)"
+    matches = re.findall(pattern, text)
+    return matches
+
+
+def extract_markdown_links(text):
+    pattern = r"\[(.*?)\]\((.*?)\)"
+    matches = re.findall(pattern, text)
+    return matches
